d9184f6b3dc9225ecdbab1eb4e3b8d1e79aee914
marcturpin91 <marcturpin19@hotmail.com>
2016-11-18 23:52:42 -0500

    start of arrayListQueriesProblem

---
+++ src/ArrayListPracticeProblem.java
@@ -0,0 +1,58 @@
+/**
+ * Created by Marc on 2016-11-18.
+ */
+public class ArrayListPracticeProblem{
+
+    /*
+    Context:
+    You are given N lines. In each line there are zero or more integers.
+    You need to answer a few queries where you need to tell the number
+    located in Yth position of Xth line.
+    Take input from System.in
+
+    Input format:
+    The first line has an integer N. In each of the next N lines there will
+    be an integer D denoting number of integers on that line and then there
+    will be D space-separated integers. In the next line there will be an
+    integer Q denoting number of queries. Each query will consist of
+    two integers X and Y.
+
+    Constraints:
+    1 <= N <= 20,000
+    0 <= D <= 50,000
+    1 <= Q <= 1,000
+    1 <= X <= N
+    ** Each number will fit in signed integer.
+    * Total number of integers in  lines will not cross 100000.
+
+    Output format:
+    In each line, output the number located in Yth position of Xth line. If there
+    is no such position, just print "ERROR!"
+
+   Sample Input/Output:
+   5
+   5 41 77 74 22 44
+   1 12
+   4 37 34 36 52
+   0
+   3 20 22 33
+   5
+   1 3
+   3 4
+   3 1
+   4 3
+   5 5
+
+   Output:
+   74
+   52
+   37
+   ERROR!
+   ERROR!
+     */
+
+    public static void main(String[] args){
+
+    }
+
+}
