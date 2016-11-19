afe7d9ac3deb73c73e75585fa8d10af85c6e0924
marcturpin91 <marcturpin19@hotmail.com>
2016-11-19 17:11:02 -0500

    started GenericsPrint challenge

---
+++ src/GenericsPrintChallenge.java
@@ -0,0 +1,45 @@
+/**
+ * Created by Marc on 2016-11-19.
+ */
+import java.io.IOException;
+import java.lang.reflect.Method;
+public class GenericsPrintChallenge{
+    /*
+    Context:
+    Let's say you have an integer array and a string array. You have
+    to write a single method printArray that can print all the elements
+    of both arrays. The method should be able to accept both integer arrays
+    or string arrays.
+
+    You are given code in the editor. Complete the code so that it
+    prints the following lines:
+    1
+    2
+    3
+    Hello
+    World
+    */
+
+        public static void main( String args[] ) {
+            Printer myPrinter = new Printer();
+            Integer[] intArray = { 1, 2, 3 };
+            String[] stringArray = {"Hello", "World"};
+            myPrinter.printArray(intArray);
+            myPrinter.printArray(stringArray);
+            int count = 0;
+
+            for (Method method : Printer.class.getDeclaredMethods()) {
+                String name = method.getName();
+
+                if(name.equals("printArray"))
+                    count++;
+            }
+
+            if(count > 1)System.out.println("Method overloading is not allowed!");
+
+        }
+        // Start solution here:
+    class Printer{
+        
+    }
+}
