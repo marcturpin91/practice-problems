02964513e7897f8d9c2b26cce8232376b4795eee
marcturpin91 <marcturpin19@hotmail.com>
2016-11-30 01:47:31 -0500

    solution for fibonacci problem

--- src/FibonacciChallenge.java
+++ src/FibonacciChallenge.java
@@ -29,7 +29,15 @@ public class FibonacciChallenge {
     public static int fibonacci(int n) {
         // Complete the function.
 
-        
+        // Base Cases
+        if(n == 0)
+            return 0;
+
+        if(n == 1)
+            return 1;
+
+        // Recursive Case
+        return fibonacci(n-1) + fibonacci(n-2);
 
     }
 
