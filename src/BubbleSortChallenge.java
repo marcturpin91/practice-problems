5889f552b3e8c2b96819d97bad0fccd0055e5703
marcturpin91 <marcturpin19@hotmail.com>
2016-11-30 01:25:28 -0500

    solution for bubble sort challenge

--- src/BubbleSortChallenge.java
+++ src/BubbleSortChallenge.java
@@ -57,8 +57,55 @@ import java.util.*;
 
 public class BubbleSortChallenge {
 
+    public static int bubbleSortSwaps(int[] a){
+        int totalNumOfSwaps = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            // Track number of elements swapped during a single array traversal
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < a.length - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    swap(a, j, j + 1);
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                return totalNumOfSwaps;
+            }
+            totalNumOfSwaps += numberOfSwaps;
+        }
+
+        return totalNumOfSwaps;
+    }
+
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int sizeOfArray = sc.nextInt();
+        sc.nextLine();
+        int[] inputArray = new int[sizeOfArray];
+        for(int i = 0; i < sizeOfArray; i++){
+            inputArray[i] = sc.nextInt();
+        }
 
+        System.out.println("Array is sorted in " + bubbleSortSwaps(inputArray) + " swaps.");
+        System.out.println("First Element: " + inputArray[0]);
+        System.out.println("Last Element: " + inputArray[sizeOfArray-1]);
+        /*
+        Array is sorted in 0 swaps.
+        First Element: 1
+        Last Element: 3
+         */
     }
 }
